c45663abb2ad3b893fbcaa1f4af22764f66280ac
Dylan Stafeil <dmstafei@syr.edu>
2025-04-27 12:46:25 -0400

    modified:   code/menuitemextractor.py

--- code/menuitemextractor.py
+++ code/menuitemextractor.py
@@ -7,13 +7,38 @@ else:
 
 
 def clean_price(price:str) -> float:
-    pass # TODO  replace with code
+    price = price.replace('$', '')
+    price = price.replace(',', '')
+    return price
 
 def clean_scraped_text(scraped_text: str) -> list[str]:
-    pass # TODO  replace with code
+    lines = scraped_text.splitlines()
+    result = []
+    for line in lines:
+        stripped_line = line.strip()
+        if not stripped_line:
+            continue
+        if stripped_line.startswith("NEW"):
+            continue
+        if stripped_line in {"GS", "V", "S", "P"}:
+            continue
+        result.append(stripped_line)
+    return result
 
-def extract_menu_item(title:str, scraped_text: str) -> MenuItem:
-    pass # TODO  replace with code
+def extract_menu_item(title: str, scraped_text: str) -> MenuItem:
+    contents = clean_scraped_text(scraped_text)
+    menu_item = MenuItem(category=title, name="", price=0.0, description="")
+    
+    if contents:
+        menu_item.name = contents[0]
+    if len(contents) > 1:
+        menu_item.price = clean_price(contents[1])
+    if len(contents) > 2:
+        menu_item.description = contents[2]
+    else:
+        menu_item.description = "No description available."
+    
+    return menu_item
 
 
 
