66a091035ba4d36d550722ff6d0c98e12b7c020a
Dylan Stafeil <dmstafei@syr.edu>
2025-05-04 12:43:47 -0400

    modified:   code/tully_scraper.py 	new file:   tullys_menu.csv

--- code/tully_scraper.py
+++ code/tully_scraper.py
@@ -11,24 +11,31 @@ def tullyscraper(playwright: Playwright) -> None:
     page.goto("https://www.tullysgoodtimes.com/menus/")
 
     extracted_items = []
-    for title in page.query_selector_all("h3.foodmenu__menu-section-title"):
+
+    titles = page.query_selector_all("h3.foodmenu__menu-section-title")
+    for title in titles:
         title_text = title.inner_text()
-        print("MENU SECTION:", title_text) 
-        row = title.query_selector("~ *").query_selector("~ *")
-        for item in row.query_selector_all("div.foodmenu__menu-item"):
-            item_text = item.inner_text()
-            extracted_item = extract_menu_item(title_text, item_text)
-            print(f"  MENU ITEM: {extracted_item.name}")
-            extracted_items.append(extracted_item.to_dict())
+        print("MENU SECTION:", title_text)
+
+        row = title.evaluate_handle("node => node.nextElementSibling?.nextElementSibling")
+
+        if row:
+            items = row.query_selector_all("div.foodmenu__menu-item")
+            for item in items:
+                item_text = item.inner_text()
+                extracted_item = extract_menu_item(title_text, item_text)
+                print(f"  MENU ITEM: {extracted_item.name}")
+                extracted_items.append(extracted_item.to_dict())
 
     df = pd.DataFrame(extracted_items)
-    df.to_csv("cache/tullys_menu.csv", index=False)    
-    
-    # ---------------------
+    df.to_csv("tullys_menu.csv", index=False)
+
     context.close()
     browser.close()
 
 
+
 with sync_playwright() as playwright:
     tullyscraper(playwright)
+
 
